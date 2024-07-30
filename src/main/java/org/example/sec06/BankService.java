b1a736b96bcb828abc89a25e8e3d139093f1ae95
YoungKyonYou <man.spride1324@gmail.com>
2024-07-30 17:41:19 +0900

    feat: gRPC streaming examples

--- src/main/java/org/example/sec06/BankService.java
+++ src/main/java/org/example/sec06/BankService.java
@@ -1,16 +1,21 @@
 package org.example.sec06;
 
+import com.google.common.util.concurrent.Uninterruptibles;
 import com.google.protobuf.Empty;
-import com.youyk.models.sec06.AccountBalance;
-import com.youyk.models.sec06.AllAccountsResponse;
-import com.youyk.models.sec06.BalanceCheckRequest;
-import com.youyk.models.sec06.BankServiceGrpc;
+import com.youyk.models.sec06.*;
 import io.grpc.stub.StreamObserver;
+import org.example.sec05.V2VersionCompatibility;
 import org.example.sec06.repository.AccountRepository;
+import org.example.sec06.requesthandlers.DepositRequestHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class BankService extends BankServiceGrpc.BankServiceImplBase {
+    private static final Logger log = LoggerFactory.getLogger(BankService.class);
+
     //이건 service 클래스이고 request와 response를 받고 보낸다
     //
     @Override
@@ -43,5 +48,42 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase {
 
         responseObserver.onNext(response);
         responseObserver.onCompleted();
+    }
+
+    //streaming 순서는 하나하나 그대로 보내진다
+    //StreamObserver는 Thread-safe하지 않다. 그래서 synchronizedList를 사용한다
+    @Override
+    public void withdraw(WithdrawReques request, StreamObserver<Money> responseObserver) {
+           /*
+            Ideally we should do some input validation. But we are going to assume only happy path scenarios.
+            Because, in gRPC, there are multiple ways to communicate the error message to the client. It has to be discussed in detail separately.
+            Assumption: account # 1 - 10 & withdraw amount is multiple of $10
+         */
+        int accountNumber = request.getAccountNumber();
+        int requestedAmount = request.getAmount();
+        Integer accountBalance = AccountRepository.getBalance(accountNumber);
+
+        if(requestedAmount > accountBalance){
+            responseObserver.onCompleted();
+            return;
+        }
+
+        for (int i = 0; i < requestedAmount / 10; i++) {
+            Money money = Money.newBuilder().setAmount(10).build();
+            responseObserver.onNext(money);
+            log.info("money sent {}", money);
+
+            AccountRepository.deductAmount(accountNumber, 10);
+            //현재 스레드를 지정된 시간 동안 일시 중지합니다. 이 경우에는 1초 동안 일시 중지합니다.
+            Uninterruptibles.sleepUninterruptibly(1, TimeUnit.SECONDS);
+        }
+        responseObserver.onCompleted();
+    }
+
+    //StreamObserver<AccountBalance> responseObserver는 outgoing message이고
+    //StreamObserver<DepositRequest> requestObserver는 incomming message이다.
+    @Override
+    public StreamObserver<DepositRequest> deposit(StreamObserver<AccountBalance> responseObserver) {
+        return new DepositRequestHandler(responseObserver);
     }
 }
---
+++ src/test/java/org/example/sec06/Lec03ServerStreamingClientTest.java
@@ -0,0 +1,49 @@
+package org.example.sec06;
+
+
+
+import com.google.protobuf.Empty;
+import com.youyk.models.sec06.*;
+import org.example.common.ResponseObserver;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Iterator;
+
+
+public class Lec03ServerStreamingClientTest extends AbstractTest{
+    private static final Logger log = LoggerFactory.getLogger(Lec03ServerStreamingClientTest.class);
+
+    @Test
+    public void blockingClientWithdrawTest() throws InterruptedException {
+        WithdrawReques request = WithdrawReques.newBuilder()
+                .setAccountNumber(2)
+                .setAmount(20)
+                .build();
+        Iterator<Money> iterator = this.blockingStub.withdraw(request);
+        int count = 0;
+        while(iterator.hasNext()){
+            log.info("received money : {}", iterator.next());
+            count++;
+        }
+        Assertions.assertEquals(2, count);
+    }
+
+    @Test
+    public void asyncClientWithdrawTest() {
+        WithdrawReques request = WithdrawReques.newBuilder()
+                .setAccountNumber(2)
+                .setAmount(20)
+                .build();
+
+        ResponseObserver<Money> observer = ResponseObserver.create();
+        this.stub.withdraw(request, observer);
+        observer.await();
+        Assertions.assertEquals(2, observer.getItems().size());
+        Assertions.assertEquals(10, observer.getItems().get(0).getAmount());
+        Assertions.assertNull(observer.getThrowable());
+    }
+
+}
